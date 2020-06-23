6e3da87e2c6f61b4e69b285f23b9a3e288193099
ksong227 <50710767+ksong227@users.noreply.github.com>
2020-06-23 19:51:47 -0400

    Add MidtermV2 (MinSDK:19,OS:4.4; TargetSDK:29,OS:10)

---
+++ MidtermV2/Source/app/src/main/java/com/example/midtermv2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.midtermv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //declare variables
+        Button button;
+        final EditText input;
+        final TextView output;
+
+        //set views
+        input = findViewById(R.id.editText);
+        output = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get dollars from EditText
+                String in = input.getText().toString();
+                //check if empty
+                if (TextUtils.isEmpty(in))
+                {
+                    input.setError("Required");
+                    return;
+                }
+                double dollars = Double.parseDouble(in);
+
+                //convert to euro
+                double euros = dollars * 0.88;
+
+                //output to TextView
+                String out = Double.toString(euros) + " Euros";
+                output.setText(out);
+            }
+        });
+    }
+}
